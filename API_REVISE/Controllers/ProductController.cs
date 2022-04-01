43aead3b5eb5ded5e232e7dcbcfda348c0feeaf9
MDABDULMALIK <abdulmalikjmi6@gmail.com>
2022-04-01 19:45:57 +0530

    Add project files.

---
+++ API_REVISE/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_REVISE.Models;
+
+namespace API_REVISE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        
+        private readonly ApplicationDbContext applicationDb;
+            public ProductController(ApplicationDbContext applicationDb)
+            {
+                this.applicationDb = applicationDb;
+            }
+            [HttpGet]
+            public IActionResult Get()
+            {
+                var products = applicationDb.Products.ToList();
+
+                return Ok(products);
+
+
+            }
+            [HttpPost]
+            public IActionResult Post(Product products)
+            {
+                applicationDb.Products.Add(products);
+                applicationDb.SaveChanges();
+                return Ok(new { ok = true, message = "product created successfully...." });
+            }
+        
+    }
+}
---
+++ API_REVISE/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_REVISE.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int quantity { get; set; }
+        public string description { get; set; }
+    }
+}
